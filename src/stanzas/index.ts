f94971067105097173bfdfff24f13a29b930a6a1
Ren-Hao Yeh <me@renhaoyeh.com>
2026-03-21 15:07:51 +0800

    feat: support get friend requests chat messages

--- src/stanzas/index.ts
+++ src/stanzas/index.ts
@@ -1,63 +1,106 @@
 import { XmppRegionObject } from '../regions';
 
 export const xmlDeclaration = (region: XmppRegionObject) =>
-  `<?xml version="1.0"?><stream:stream to="${region.domain}.pvp.net" version="1.0" xmlns:stream="http://etherx.jabber.org/streams">`;
+	`<?xml version="1.0"?><stream:stream to="${region.domain}.pvp.net" version="1.0" xmlns:stream="http://etherx.jabber.org/streams">`;
 
 export const mechanism = (authToken: string, pasToken: string) => ({
-  auth: {
-    '@_mechanism': 'X-Riot-RSO-PAS',
-    '@_xmlns': 'urn:ietf:params:xml:ns:xmpp-sasl',
-    rso_token: authToken,
-    pas_token: pasToken,
-  },
+	auth: {
+		'@_mechanism': 'X-Riot-RSO-PAS',
+		'@_xmlns': 'urn:ietf:params:xml:ns:xmpp-sasl',
+		rso_token: authToken,
+		pas_token: pasToken,
+	},
 });
 
 export const clientName = () => ({
-  iq: {
-    '@_id': '_xmpp_bind1',
-    '@_type': 'set',
-    bind: {
-      '@_xmlns': 'urn:ietf:params:xml:ns:xmpp-bind',
-      'puuid-mode': {
-        '@_enabled': 'true',
-      },
-      resource: 'RC-VALORANT-NODE',
-    },
-  },
+	iq: {
+		'@_id': '_xmpp_bind1',
+		'@_type': 'set',
+		bind: {
+			'@_xmlns': 'urn:ietf:params:xml:ns:xmpp-bind',
+			'puuid-mode': {
+				'@_enabled': 'true',
+			},
+			resource: 'RC-VALORANT-NODE',
+		},
+	},
+});
+
+export const setEntitlements = (entitlementsToken: string) => new Object({
+	'iq': {
+		'@_id': "xmpp_entitlements_0",
+		'@_type': "set",
+		'entitlements': {
+			'@_xmlns': "urn:riotgames:entitlements",
+			'token': entitlementsToken
+		}
+	}
 });
 
 export const rxep = () => ({
-  iq: {
-    '@_id': 'set_rxep_1',
-    '@_type': 'set',
-    rxcep: {
-      '@_xmlns': 'urn:riotgames:rxep',
-      'last-online-state': {
-        '@_enabled': 'true',
-      },
-    },
-  },
+	iq: {
+		'@_id': 'set_rxep_1',
+		'@_type': 'set',
+		rxcep: {
+			'@_xmlns': 'urn:riotgames:rxep',
+			'last-online-state': {
+				'@_enabled': 'true',
+			},
+		},
+	},
 });
 
 export const setupSession = () => ({
-  iq: {
-    '@_id': '_xmpp_session1',
-    '@_type': 'set',
-    session: {
-      '@_xmlns': 'urn:ietf:params:xml:ns:xmpp-session',
-    },
-  },
+	iq: {
+		'@_id': '_xmpp_session1',
+		'@_type': 'set',
+		session: {
+			'@_xmlns': 'urn:ietf:params:xml:ns:xmpp-session',
+		},
+	},
 });
 
 export const fetchFriends = () => ({
-  iq: {
-    '@_type': 'get',
-    query: {
-      '@_xmlns': 'jabber:iq:riotgames:roster',
-    },
-  },
+	iq: {
+		'@_type': 'get',
+		query: {
+			'@_xmlns': 'jabber:iq:riotgames:roster',
+		},
+	},
+});
+
+
+export const sendFriendRequest = (username: string, tagline: string) => new Object({
+	'iq': {
+		'@_type': "set",
+		'@_id': "roster_add_10",
+		'query': {
+			'@_xmlns': "jabber:iq:riotgames:roster",
+			'item': {
+				'@_subscription': "pending_out",
+				'id': {
+					'@_name': username,
+					'@_tagline': tagline
+				}
+			}
+		}
+	}
+});
+
+export const removeOutgoingFriendRequest = (jid: string) => new Object({
+	'iq': {
+		'@_type': "set",
+		'@_id': "roster_remove_1",
+		'query': {
+			'@_xmlns': "jabber:iq:riotgames:roster",
+			'item': {
+				'@_subscription': "remove",
+				'@_jid': jid
+			}
+		}
+	}
 });
 
 export const presence = (presenceData: any = {}) => ({
-  presence: presenceData,
+	presence: presenceData,
 });
